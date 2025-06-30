8681f93c240481e7e5fd87b1c644547286091a0c
Han-Teng Liao <h.liao@ieee.org>
2025-06-30 10:25:21 +0800

    main.css h1-h5

--- tailwind.config.js
+++ tailwind.config.js
@@ -115,6 +115,10 @@ module.exports = {
         lg: '1.125rem',
         xl: '1.25rem',
         '2xl': '1.5rem',
+        '3xl': '1.875rem',
+        // '4xl': '2.25rem',
+        // '5xl': '3rem',
+        // '6xl': '4rem'
         // '3xl': '1.875rem',
         // '4xl': '2.25rem',
         // '5xl': '3rem',
